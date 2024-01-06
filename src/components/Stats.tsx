eaa4e634b107227c083e5b51d8f6d8d1807a82cf
macbookpro <mahsinsaifullah@gmail.com>
2024-01-06 16:40:08 +0000

    feat: add ui for empty list

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -9,11 +9,19 @@ export const Stats: React.FC<StatsProps> = ({
   totalPackedItems,
   percentageOfItemsPacked,
 }) => {
+  if (!totalItems) {
+    <p className="stats">
+      <em>Start adding some items to your packing list 🚀</em>
+    </p>;
+  }
+
   return (
     <footer className="stats">
       <em>
-        💼 You have {totalItems} items on your list, and you already packed{" "}
-        {totalPackedItems} ({percentageOfItemsPacked}%)
+        {percentageOfItemsPacked === 100
+          ? 'You got everything! Ready to go ✈️'
+          : `💼 You have ${totalItems} items on your list, and you already packed
+          ${totalPackedItems} (${percentageOfItemsPacked}%)`}
       </em>
     </footer>
   );
